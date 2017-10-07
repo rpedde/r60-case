6dede8df07e9e1cb391cc597c774336e72587aae
rpedde <ron@pedde.com>
2017-10-07 15:42:32 -0500

    name output files based on size dependence

--- build.sh
+++ build.sh
@@ -9,22 +9,28 @@ fi
 function gen() {
     local what=$1
     local format=$2
+    local varies=$3
 
     optargs=""
     optfname=""
 
     if [ "${HEIGHT:-}" != "" ]; then
         optargs="-D stock_height=$HEIGHT"
-        optfname="-${HEIGHT}"
+        if [ "${varies}" == "yes" ]; then
+          optfname="-${HEIGHT}"
+        fi
     fi
 
     openscad -o r60-${what}${optfname}.${format} -D "mod=\"${what}\"" ${optargs} r60.scad
 }
 
-for d in case:stl holes:dxf usb:dxf standoff:stl bottom:dxf feet:dxf clamps:stl sidecut:dxf; do
-    what=${d%:*}
-    format=${d#*:}
+for d in case:stl:yes holes:dxf:no usb:dxf:no standoff:stl:yes bottom:dxf:no feet:dxf:no clamps:stl:yes sidecut:dxf:no; do
+    items=($(echo ${d} | tr ":" " "))
+
+    what=${items[0]}
+    format=${items[1]}
+    varies_by_height=${items[2]}
 
     echo "Generating ${what} (format: ${format})"
-    gen $what $format
+    gen $what $format $varies_by_height
 done
